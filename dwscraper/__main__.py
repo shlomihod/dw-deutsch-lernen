e403b813e018cc85b048e23c47b061ab7bc06cee
shlomi_h <hod_s@bezeqint.net>
2019-01-01 22:50:47 +0100

    adding main script

--- dwscraper/__main__.py
+++ dwscraper/__main__.py
@@ -1,3 +1,21 @@
+from dwscraper import scrap_dw, save_dataframes
+from dwscraper.consts import DEFAULT_NUM_PARALLEL_REQUESTS
+
+
+def main(path: "Path to save dataframes" = ".",
+         n_pages: "Number of pages (~articles) to sample (integer or 'all'" = 'all',
+         verbose: ("Whether to print additional information", "flag", "v") = False,
+         n_parallel_requests: ("Number of parallel download requests", "option", "r") = DEFAULT_NUM_PARALLEL_REQUESTS):
+
+         
+    "Scrap Deutsch Lernen articles from DW website."
+
+    n_pages = None if n_pages == 'all' else int(n_pages)
+
+    page_df, text_df, paragraph_df = scrap_dw(n_pages,
+                                              n_parallel_requests,
+                                              verbose)
+
     save_dataframes(path, page_df, text_df, paragraph_df)
 
 
